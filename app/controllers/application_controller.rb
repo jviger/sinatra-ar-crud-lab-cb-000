c3e06c598f6660345a6a48e13407da3e1fa9928c
Jason Viger <jason.viger@gmail.com>
2018-08-30 10:42:45 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,46 @@ class ApplicationController < Sinatra::Base
   get '/' do
 
   end
+  
+  get '/posts/new' do
+  
+    erb :new
+  end
+  
+  post '/posts' do
+  
+    @blog_post = Post.create(params)
+    redirect to '/posts'
+  end
+  
+  get '/posts' do
+    @posts = Post.all
+    erb :index
+  end 
+  
+  get '/posts/:id' do
+    @blog_post = Post.find_by_id(params[:id])
+    erb :show
+  end
+  
+  get '/posts/:id/edit' do
+    @blog_id = params[:id]
+    @blog_post = Post.find_by_id(params[:id])
+    erb:edit
+  end
+  
+  patch '/posts/:id' do
+    @blog_post = Post.find_by_id(params[:id])
+    @blog_post.name = params[:name]
+    @blog_post.content = params[:content]
+    @blog_post.save
+    erb :show
+  end
+  
+  delete '/posts/:id/delete' do
+    @blog_post = Post.find_by_id(params[:id])
+    @blog_post.delete
+    erb :deleted
+  end
+  
 end
